8d279b4cd412fd911a0c32139b20e6ad51542ce0
alireza10hp <45386926+alireza10hp@users.noreply.github.com>
2019-05-05 23:01:40 +0430

    4 exception

---
+++ src/Exceptions/InvalidAttackException.java
@@ -0,0 +1,5 @@
+package Exceptions;
+
+public class InvalidAttackException extends RuntimeException {
+    public InvalidAttackException(String message){super(message);}
+}
---
+++ src/Exceptions/InvalidTargetException.java
@@ -0,0 +1,5 @@
+package Exceptions;
+
+public class InvalidTargetException extends RuntimeException {
+    public InvalidTargetException(String message){super(message);}
+}
